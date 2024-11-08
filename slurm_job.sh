f5f46b3858d5840eb8b64e4a21e61d05b766fcf2
Gabriel <gabriel.villamil@mdc-berlin.de>
2024-11-08 08:31:05 +0100

    Run Ribostan and Salmon jobs

--- slurm_job.sh
+++ slurm_job.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-#SBATCH --job-name=orfquant
+#SBATCH --job-name=hct116_anno_orfquant_reports
 #SBATCH --mail-user=gabriel.villamil@mdc-berlin.de
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --output=logs/snakemake/%j.snakemake.stdout.log
 #SBATCH --export=ALL
 #SBATCH --chdir=.
-#SBATCH --ntasks=60
+#SBATCH --ntasks=15
 #SBATCH --time=168:00:00
-#SBATCH --mem-per-cpu=10000MB
+#SBATCH --mem-per-cpu=30000MB
 
 
 # Source .bashrc file
@@ -18,4 +18,4 @@ source ~/.bashrc
 conda activate z_snakemake
 
 # Run Snakemake
-snakemake -j 60 -k -p --restart-times 1 --max-jobs-per-second 5 --rerun-incomplete --use-singularity
+snakemake -j 15 -k -p --restart-times 1 --max-jobs-per-second 5 --rerun-incomplete --use-singularity
