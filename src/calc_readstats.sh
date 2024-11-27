1e26f7a2579cf62ba8f2162c51f793c5710fadfd
Gabriel <gabriel.villamil@mdc-berlin.de>
2024-11-27 16:45:55 +0100

    Update naming in calc_readstats.sh

--- src/calc_readstats.sh
+++ src/calc_readstats.sh
@@ -3,12 +3,12 @@
 
 
 
-rm -rf results/post/readstats.csv
+rm -rf results/post/readstats_ribo.csv
 
-echo "sample,raw_reads,cutadapt_reads,collapse_reads,trim_reads,filter_reads,reads_mapped" > results/post/readstats.csv
+echo "sample,raw_reads,cutadapt_reads,collapse_reads,trim_reads,filter_reads,reads_mapped" > results/post/readstats_ribo.csv
 
 
-for sample in $(tail -n +2 config/samples.csv)
+for sample in $(tail -n +2 config/ribo_samples.csv)
 do
 	var0=$(echo $(zcat data/riboseq/$sample.fastq.gz | wc -l)/4|bc)
 	var1=$(echo $(zcat results/cutadapt_reads/$sample.fastq.gz | wc -l)/4|bc)
@@ -17,5 +17,5 @@ do
 	var4=$(echo $(zcat results/filter_reads/$sample/$sample.fastq.gz | wc -l)/4|bc)
 	var5=$(awk $'/SN\treads mapped:/{ print $4 }' results/star/genome/reports/$sample/$sample.bamstats.txt)
 
-	echo "$sample,$var0,$var1,$var2,$var3,$var4,$var5" >> results/post/readstats.csv
+	echo "$sample,$var0,$var1,$var2,$var3,$var4,$var5" >> results/post/readstats_ribo.csv
 done
