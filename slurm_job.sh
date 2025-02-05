d14e690a6fd85a682bf9f46a5ef6bcfb4d19fe81
Gabriel <gabriel.villamil@mdc-berlin.de>
2025-02-05 16:50:31 +0100

    Use primary alignments when mapping RNA-seq reads

--- slurm_job.sh
+++ slurm_job.sh
@@ -1,25 +1,25 @@
 #! /bin/bash
 
-#SBATCH --job-name=extract_multimappers_rnaseq
+#SBATCH --job-name=map_transcriptome_primary_rnaseq_eif3d
 #SBATCH --mail-user=gabriel.villamil@mdc-berlin.de
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --output=logs/snakemake/%j.snakemake.stdout.log
 #SBATCH --export=ALL
 #SBATCH --chdir=.
-#SBATCH --ntasks=1
+#SBATCH --ntasks=24
 #SBATCH --time=168:00:00
-#SBATCH --mem-per-cpu=256000MB
+#SBATCH --mem-per-cpu=20000MB
 
 
 # Source .bashrc file
 source ~/.bashrc
 
 # Activate Snakemake conda environment
-# conda activate z_snakemake
+conda activate z_snakemake
 
 # Run Snakemake
-# snakemake -j 1 -k -p --restart-times 1 --max-jobs-per-second 5 --rerun-incomplete --use-singularity
+snakemake -j 24 -k -p --restart-times 1 --max-jobs-per-second 5 --rerun-incomplete --use-singularity
 
 # Call another bash script
-conda activate ribopipe
-bash src/extract_multimappers_rnaseq.sh
+# conda activate ribopipe
+# bash src/extract_multimappers_rnaseq.sh
